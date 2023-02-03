d8ba2f4f22507651378f4c63a7c0c740cbcadf6e
Huyen412 <ngochuyengiadinh4@gmail.orgg>
2023-02-03 16:27:53 +0700

    deploy 1

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,3 +1,3 @@
 export const STATIC_HOST="https://manager-app-uef5.onrender.com/"
-export const HOST="https://tfi.vercel.app/"
+export const HOST="https://tfi-4db4lci00-nguyenngochuyen1234.vercel.app/"
 
