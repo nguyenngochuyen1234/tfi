20f190cd7561a353f022a651694f3ed5ad45a7b7
ThangDevAlone <quangthangvtlg@gmail.com>
2023-02-17 22:21:02 +0700

    thang

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,3 +1,3 @@
-export const STATIC_HOST="https://manager-app-uef5.onrender.com/"
+export const STATIC_HOST="https://localhost:8000/"
 export const HOST="https://tfi-git-main-nguyenngochuyen1234.vercel.app/"
 
